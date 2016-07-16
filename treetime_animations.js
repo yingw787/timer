9a9b7455b01992be311efbdfe7ffd2a19501cbd8
Ying Wang <yingw787@gmail.com>
2016-07-16 00:09:13 -0400

    Added working jQuery dial, trees transition, need to work on notifications, text, and remaining event handling.

--- treetime_animations.js
+++ treetime_animations.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
 	$(introText).delay(3000).fadeOut(1000);
 	$(timerDial).hide().delay(4000).fadeIn(1000);
+	$(timerTreeDisplay).hide();
+
+	$(setTimerButton).click(function(){
+		$(timerDial).fadeOut(1000);
+		$(timerTreeDisplay).delay(1000).fadeIn(1000);
+	})
+
+	$(showTreesButton).click(function(){
+		$(treeAliveImage).fadeIn(1000).delay(1000).fadeOut(1000);
+		$(treeDeadImage).delay(2000).fadeIn(1000);
+
+	})
+
 })
+
+
